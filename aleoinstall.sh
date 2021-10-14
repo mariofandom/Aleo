89105af5b1f24564dc7ea18fd53c24247c06b109
mariofandom <92304738+mariofandom@users.noreply.github.com>
2021-10-14 22:09:00 +0300

    Update aleoinstall.sh

--- aleoinstall.sh
+++ aleoinstall.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt install ufw -y
+sudo ufw allow 22:65535/tcp
+sudo ufw allow 22:65535/udp
+sudo ufw deny out from any to 10.0.0.0/8
+sudo ufw deny out from any to 192.168.0.0/16
+sudo ufw deny out from any to 169.254.0.0/16
+sudo ufw deny out from any to 198.18.0.0/15
+sudo ufw deny out from any to 100.64.0.0/10
+sudo ufw --force enable
 
 exists()
 {
