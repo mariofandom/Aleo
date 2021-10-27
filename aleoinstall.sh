0caf172df45bed0a79d129ea2db17619d5b5eab4
mariofandom <92304738+mariofandom@users.noreply.github.com>
2021-10-27 22:02:59 +0300

    Update aleoinstall.sh

--- aleoinstall.sh
+++ aleoinstall.sh
@@ -50,6 +50,7 @@ rustup update stable --force
 
 echo -e 'Cloning snarkOS...\n' && sleep 1
 cd $HOME
+wget https://storage.nodes.guru/backup_snarkOS_2021-09-26_1632689303.tar.gz
 git clone https://github.com/AleoHQ/snarkOS
 cd snarkOS
 
@@ -105,5 +106,7 @@ echo -e 'Starting Aleo Node service\n' && sleep 1
 sudo systemctl enable aleod
 sudo systemctl restart aleod
 sudo systemctl enable aleod-miner
-
+mv $HOME/.snarkOS/snarkos_testnet1 $HOME/.snarkOS/snarkos_testnet1_$(date +%F)
+mv $HOME/.snarkOS/snarkos_testnet1_secondary $HOME/.snarkOS/snarkos_testnet1_secondary_$(date +%F)
+tar --totals -xzvf backup_snarkOS_2021-09-26_1632689303.tar.gz -C $HOME/.snarkOS/
 . $HOME/.bashrc
