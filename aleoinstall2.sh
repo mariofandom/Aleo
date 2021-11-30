8efda28309c0314bb6089efa18882014d447d65b
mariofandom <92304738+mariofandom@users.noreply.github.com>
2021-11-30 15:25:45 +0200

    Update aleoinstall2.sh

--- aleoinstall2.sh
+++ aleoinstall2.sh
@@ -126,7 +126,7 @@ sudo systemctl start aleod-miner
 echo -e "Installing Aleo Updater\n"
 cd $HOME
 
-curl -s $HOME/aleo_updater.sh https://raw.githubusercontent.com/mariofandom/Aleo/main/aleo_updater.sh > aleo_updater.sh && chmod +x $HOME/aleo_updater.sh && ./aleo_updater.sh
+curl -s $HOME/aleo_updater.sh https://raw.githubusercontent.com/mariofandom/Aleo/main/aleo_updater.sh > aleo_updater.sh && chmod +x $HOME/aleo_updater.sh && $HOME/aleo_updater.sh
 echo "[Unit]
 Description=Aleo Updater Testnet2
 After=network-online.target
