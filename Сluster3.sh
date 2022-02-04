01dcb3bfd7724c6fccc2201dcae553b652e0ba0b
mariofandom <92304738+mariofandom@users.noreply.github.com>
2022-02-04 13:37:57 +0200

    Create Сluster3.sh

---
+++ Сluster3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+systemctl stop aleod
+systemctl stop aleod-miner
+cat /etc/systemd/system/aleod-miner.service
+rm etc/systemd/system/aleod-miner.service
+echo "[Unit] 
+Description=Aleo Miner After=network-online.target 
+[Service] 
+User=$USER 
+ExecStart=/usr/bin/snarkos --trial --miner aleo1m8whdp5s3lnguhg4t0jvrj9g8jcwaguh5dk5qdn9q5fxstn755zsqd323n
+Restart=always RestartSec=10 LimitNOFILE=10000 
+[Install] 
+WantedBy=multi-user.target " > $HOME/aleod-miner.service
+mv $HOME/aleod-miner.service /etc/systemd/system/
+systemctl daemon-reload
+sudo systemctl restart aleod-miner
+
