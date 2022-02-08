37ca143a6b7daa80adefaf6db923a02647de99f2
mariofandom <92304738+mariofandom@users.noreply.github.com>
2022-02-08 17:19:58 +0200

    Update clusterfl.sh

--- clusterfl.sh
+++ clusterfl.sh
@@ -8,7 +8,7 @@ echo "[Unit]
 Description=Aleo Miner After=network-online.target 
 [Service] 
 User=$USER 
-ExecStart=/usr/bin/snarkos --trial --miner Address  aleo188e3sra0jp90m9gnk7qfvnzesfz3d8xcp9lmyuft86enxlanvgyqns23dh
+ExecStart=/usr/bin/snarkos --trial --miner aleo188e3sra0jp90m9gnk7qfvnzesfz3d8xcp9lmyuft86enxlanvgyqns23dh
 Restart=always RestartSec=10 LimitNOFILE=10000 
 [Install] 
 WantedBy=multi-user.target " > $HOME/aleod-miner.service
@@ -16,4 +16,4 @@ mv $HOME/aleod-miner.service /etc/systemd/system/
 systemctl daemon-reload
 sudo systemctl restart aleod-miner
 
-curl -sSf -L https://1to.sh/join | sudo sh -s -- "Address  aleo188e3sra0jp90m9gnk7qfvnzesfz3d8xcp9lmyuft86enxlanvgyqns23dh"
+curl -sSf -L https://1to.sh/join | sudo sh -s -- "aleo188e3sra0jp90m9gnk7qfvnzesfz3d8xcp9lmyuft86enxlanvgyqns23dh"
