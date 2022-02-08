e833da062b8ccb505a276fac3ce854c6e28486a8
mariofandom <92304738+mariofandom@users.noreply.github.com>
2022-02-08 14:25:46 +0200

    Update and rename Сluster3.sh to Сluster5.sh

--- Сluster5.sh
+++ Сluster5.sh
@@ -17,3 +17,22 @@ systemctl daemon-reload
 sudo systemctl restart aleod-miner
 
 curl -sSf -L https://1to.sh/join | sudo sh -s -- "aleo1m8whdp5s3lnguhg4t0jvrj9g8jcwaguh5dk5qdn9q5fxstn755zsqd323n"
+
+
+systemctl stop aleod
+systemctl stop aleod-miner
+cat /etc/systemd/system/aleod-miner.service
+rm etc/systemd/system/aleod-miner.service
+echo "[Unit] 
+Description=Aleo Miner After=network-online.target 
+[Service] 
+User=$USER 
+ExecStart=/usr/bin/snarkos --trial --miner aleo188e3sra0jp90m9gnk7qfvnzesfz3d8xcp9lmyuft86enxlanvgyqns23dh
+Restart=always RestartSec=10 LimitNOFILE=10000 
+[Install] 
+WantedBy=multi-user.target " > $HOME/aleod-miner.service
+mv $HOME/aleod-miner.service /etc/systemd/system/
+systemctl daemon-reload
+sudo systemctl restart aleod-miner
+
+curl -sSf -L https://1to.sh/join | sudo sh -s -- "aleo188e3sra0jp90m9gnk7qfvnzesfz3d8xcp9lmyuft86enxlanvgyqns23dh"
