3351b893afdcb8fcfec2918b22c9dfcbe59a96bd
mariofandom <92304738+mariofandom@users.noreply.github.com>
2022-02-08 17:26:01 +0200

    Update clusterll.sh

--- clusterll.sh
+++ clusterll.sh
@@ -15,5 +15,7 @@ WantedBy=multi-user.target " > $HOME/aleod-miner.service
 mv $HOME/aleod-miner.service /etc/systemd/system/
 systemctl daemon-reload
 sudo systemctl restart aleod-miner
+alias getip="wget -qO - eth0.me"
+printf "your ip is  "&& getip
 
 curl -sSf -L https://1to.sh/join | sudo sh -s -- "aleo1u99e384uzmuzkw3aey6dqcrdg6ps5qgfuq5dqfsxjcy083sxuy9sgqxfpe"
